d1343cf0b6917ae630f54ea9b3888671c376ef18
Aurélien CARLE <aurelien.carle@cc.in2p3.fr>
2022-07-26 23:30:50 +0200

    upgrade tests

--- examples/test_geometry.cpp
+++ examples/test_geometry.cpp
@@ -1,11 +1,12 @@
 #include "geometry/point.hpp"
 #include <iostream>
+#include <string>
 
 template <typename T>
-void print(geometry::Point<T> p)
+void print(geometry::Point<T> p, std::string const message)
 {
     std::cout << "2D point : ";
-    std::cout << "(" << p.x << ":" << p.y << ")" << std::endl;
+    std::cout << "(" << p.x << "," << p.y << ") --> " << message << std::endl;
 }
 
 int main()
@@ -13,9 +14,21 @@ int main()
 
     geometry::Point<int> base(2, 3);
 
-    print(base);
-    print(base + geometry::Point(-2, 6));
-    print(base * 2.3);
+    print(base, "(2,3)");
+    print(base + geometry::Point(-2, 6), "(0,9)");
+    print(base * 2.3, "(4.6,6.9)");
+
+    base += geometry::Point(1, -1);
+
+    print(base, "(3,2)");
+    print(base + geometry::Point(-2, 6), "(1,8)");
+    print(base * 2.3, "(6.9, 4.6)");
+
+    base *= 0;
+
+    print(base, "(0,0)");
+    print(base + geometry::Point(-2, 6), "(-2,6)");
+    print(base * 2.3, "(0,0)");
 
     return 0;
 }
--- src/geometry/point.hpp
+++ src/geometry/point.hpp
@@ -23,28 +23,126 @@ struct Point {
     }
 };
 
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param right
+ * @return constexpr Point<T>
+ */
 template <typename T>
 constexpr Point<T> operator-(const Point<T> &right)
 {
     return Point<T>(-right.x, -right.y);
 }
 
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<T>
+ */
 template <typename T>
 constexpr Point<T> operator+(const Point<T> &left, const Point<T> &right)
 {
     return Point<T>(left.x + right.x, left.y + right.y);
 }
 
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<T>
+ */
 template <typename T>
 constexpr Point<T> operator-(const Point<T> &left, const Point<T> &right)
 {
     return Point<T>(left.x - right.x, left.y - right.y);
 }
 
+/**
+ * @brief
+ *
+ * @tparam U
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<U>
+ */
 template <typename U, typename T>
 constexpr Point<U> operator*(const Point<T> &left, U right)
 {
     return Point<U>(left.x * right, left.y * right);
+}
+
+/**
+ * @brief
+ *
+ * @tparam U
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<U>
+ */
+template <typename U, typename T>
+constexpr Point<U> operator*(U left, const Point<T> &right)
+{
+    return Point<U>(left * right.x, left * right.y);
+}
+
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<T>&
+ */
+template <typename T>
+constexpr Point<T> &operator+=(Point<T> &left, const Point<T> &right)
+{
+    left.x += right.x;
+    left.y += right.y;
+
+    return left;
+}
+
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<T>&
+ */
+template <typename T>
+constexpr Point<T> &operator*=(Point<T> &left, T right)
+{
+    left.x *= right;
+    left.y *= right;
+
+    return left;
+}
+
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param left
+ * @param right
+ * @return constexpr Point<T>&
+ */
+template <typename T>
+constexpr Point<T> &operator-=(Point<T> &left, const Point<T> &right)
+{
+    left.x -= right.x;
+    left.y -= right.y;
+
+    return left;
 }
 
 } // namespace geometry
