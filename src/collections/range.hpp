a22f22ac490590bb6a75ebbc32228418c19574da
GregoireUhlrich <greg.um@free.fr>
2022-08-02 22:20:30 +0200

    range.hpp

---
+++ src/collections/range.hpp
@@ -0,0 +1,54 @@
+#ifndef CPT_RANGE_H_INCLUDED
+#define CPT_RANGE_H_INCLUDED
+
+#include "collection.hpp"
+#include "../utils/error.hpp"
+#include <numeric>
+#include <algorithm>
+
+namespace cpt
+{
+    class InvalidRangeError: public Exception {
+        using Exception::Exception;
+    };
+
+    template<std::integral T = int>
+    Collection<T> range(
+        T start,
+        T end,
+        T step)   
+    {
+        if (step == 0) {
+            throw InvalidRangeError("Cannot create a range with a 0 step.");
+        }
+        if ((end - start)*step < 0) {
+            throw InvalidRangeError(
+                "Step ", step, 
+                " is invalid (wrong sign) for range [", 
+                start, ", ", end, ").");
+        }
+        Collection<T> range((end - start) / step);
+        std::ranges::generate(range, [value = start, step=step]() mutable  { 
+            const auto res = value;
+            value += step;
+            return res;
+        });
+        return range; 
+    }
+
+    template<std::integral T = int>
+    Collection<T> range(
+        T start,
+        T end)
+    {
+        return range<T>(start, end, 1);
+    }   
+
+    template<std::integral T = int>
+    Collection<T> range(T end)
+    {
+        return range<T>(0, end);
+    }   
+} // namespace cpt
+
+#endif
