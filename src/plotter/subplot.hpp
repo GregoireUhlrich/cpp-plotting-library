fe1cf02f6de4eb22b11cde192cc94cfc93d8dd8b
GregoireUhlrich <greg.um@free.fr>
2022-08-08 16:55:44 +0200

    Basic Subplot object

---
+++ src/plotter/subplot.hpp
@@ -0,0 +1,12 @@
+#ifndef CPT_SUBPLOT_H_INCLUDED
+#define CPT_SUBPLOT_H_INCLUDED
+
+namespace cpt
+{
+    class SubPlot {
+        SubPlot(SubPlot const &) = delete;
+        SubPlot(SubPlot &&) noexcept = default;
+    };
+} // namespace cpt
+
+#endif
