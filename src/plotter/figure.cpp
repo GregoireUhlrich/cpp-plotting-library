ea2d42958ea451594cd045755808fd9f39e87e41
GregoireUhlrich <greg.um@free.fr>
2022-08-08 17:02:02 +0200

    Figure implementation + shared libs

--- src/plotter/figure.cpp
+++ src/plotter/figure.cpp
@@ -3,5 +3,82 @@
 
 namespace cpt
 {
-    
+    static unsigned int pixels_per_inch()
+    {
+        return 96;
+    }
+
+    Figure::Figure(std::string_view name, unsigned int width, unsigned int height)
+        : _name(name),
+            _width(width),
+            _height(height)
+    {
+
+    }
+
+    Figure::Figure(unsigned int width, unsigned int height)
+        : Figure("Figure " + std::to_string(++n_figures), width, height)
+    {
+
+    }
+
+    Figure::~Figure()
+    {
+        if (_execution_result.valid()) {
+            _execution_result.wait();
+        }
+    }
+
+    bool Figure::is_blocking() const noexcept {
+        return _blocking;
+    }
+
+    void Figure::set_blocking(bool blocking) noexcept
+    {
+        _blocking = blocking;
+    }
+
+    void Figure::show()
+    {
+        if (_execution_result.valid()) {
+            throw FigureConcurrencyError(
+                "Cannot show a figure twice (tried for \"",
+                _name, "\")!");
+        }
+        _execution_result = std::async(std::launch::async, &Figure::launch, this);
+        if (_blocking) {
+            _execution_result.wait();
+        }
+    }
+
+    void Figure::launch()
+    {
+        std::lock_guard<std::mutex> guard(_mutex);
+        Figure::create_window(
+            _window,
+            sf::VideoMode(_width*pixels_per_inch(), _height*pixels_per_inch()),
+            _name,
+            sf::Style::Close | sf::Style::Titlebar
+            );
+        while (_window.isOpen()) {
+            sf::Event event;
+            while (_window.waitEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    _window.close();
+            }
+        }
+    }
+
+    void Figure::create_window(
+        sf::RenderWindow          &window,
+        sf::VideoMode       const &mode,
+        std::string         const &name,
+        uint32_t                   style,
+        sf::ContextSettings const &settings)
+    {
+        static std::mutex _mutex;
+        std::lock_guard<std::mutex> lock(_mutex);
+        window.create(mode, name, style, settings);
+    }
+
 } // namespace cpt
--- src/plotter/figure.hpp
+++ src/plotter/figure.hpp
@@ -9,11 +9,6 @@
 
 namespace cpt
 {
-    static unsigned int pixels_per_inch()
-    {
-        return 96;
-    }
-
     class FigureConcurrencyError: public cpt::Exception {
         using cpt::Exception::Exception;
     };
@@ -22,84 +17,32 @@ namespace cpt
     
     public:
 
-        Figure(std::string_view name, unsigned int width, unsigned int height)
-            : _name(name),
-              _width(width),
-              _height(height)
-        {
+        Figure(std::string_view name, unsigned int width, unsigned int height);
 
-        }
-
-        Figure(unsigned int width, unsigned int height)
-            : Figure("Figure " + std::to_string(++n_figures), width, height)
-        {
-
-        }
+        Figure(unsigned int width, unsigned int height);
 
         Figure(Figure const &) = delete;
 
         Figure(Figure &&) = default;
 
-        ~Figure()
-        {
-            if (_execution_result.valid()) {
-                _execution_result.wait();
-            }
-        }
+        ~Figure();
 
-        bool is_blocking() const noexcept {
-            return _blocking;
-        }
+        bool is_blocking() const noexcept;
 
-        void set_blocking(bool blocking) noexcept
-        {
-            _blocking = blocking;
-        }
+        void set_blocking(bool blocking) noexcept;
 
-        void show()
-        {
-            if (_execution_result.valid()) {
-                throw FigureConcurrencyError(
-                    "Cannot show a figure twice (tried for \"",
-                    _name, "\")!");
-            }
-            _execution_result = std::async(std::launch::async, &Figure::launch, this);
-            if (_blocking) {
-                _execution_result.wait();
-            }
-        }
+        void show();
 
     protected:
 
-        void launch()
-        {
-            std::lock_guard<std::mutex> guard(_mutex);
-            Figure::create_window(
-                _window,
-                sf::VideoMode(_width*pixels_per_inch(), _height*pixels_per_inch()),
-                _name,
-                sf::Style::Close | sf::Style::Titlebar
-                );
-            while (_window.isOpen()) {
-                sf::Event event;
-                while (_window.waitEvent(event)) {
-                    if (event.type == sf::Event::Closed)
-                        _window.close();
-                }
-            }
-        }
+        void launch();
 
         static void create_window(
             sf::RenderWindow          &window,
             sf::VideoMode       const &mode,
             std::string         const &name,
             uint32_t                   style,
-            sf::ContextSettings const &settings = sf::ContextSettings())
-        {
-            static std::mutex _mutex;
-            std::lock_guard<std::mutex> lock(_mutex);
-            window.create(mode, name, style, settings);
-        }
+            sf::ContextSettings const &settings = sf::ContextSettings());
 
     private:
         std::string       _name;
@@ -113,7 +56,6 @@ namespace cpt
     private:
         static inline unsigned int n_figures = 0;
     };
-
 } // namespace cpt
 
 
