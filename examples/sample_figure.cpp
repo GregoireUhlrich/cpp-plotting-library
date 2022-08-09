87cb29ccf8f6edcb22a5873b5ab1f41cb0d2c621
GregoireUhlrich <greg.um@free.fr>
2022-08-09 16:38:34 +0200

    Modified sample_figure and build procedure

--- examples/sample_figure.cpp
+++ examples/sample_figure.cpp
@@ -7,6 +7,11 @@ int main()
     cpt::Figure fign("My Fig", 10, 5);
     cpt::Figure fig2(5, 5);
     fign.set_blocking(true);
+    fign.create_subplots(2, 2);
+    cpt::Subplot &s1 = fign.get_subplot(0, 0);
+    cpt::Subplot &s2 = fign.get_subplot(0, 1);
+    cpt::Subplot &s3 = fign.get_subplot(1, 0);
+    cpt::Subplot &s4 = fign.get_subplot(1, 1);
 
     fig1.show();
     std::cout << "Figure 1 and My Fig should be there but that's all" << std::endl; 
