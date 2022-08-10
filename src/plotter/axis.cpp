e866c7822ad74b0f35e50dbf574986aec2ebef22
GregoireUhlrich <greg.um@free.fr>
2022-08-10 18:28:29 +0200

    axis files, first commit

---
+++ src/plotter/axis.cpp
@@ -0,0 +1,6 @@
+#include "axis.hpp"
+
+namespace cpt
+{
+    
+} // namespace cpt
---
+++ src/plotter/axis.hpp
@@ -0,0 +1,12 @@
+#ifndef CPT_AXIS_H_INCLUDED
+#define CPT_AXIS_H_INCLUDED
+
+#include <SFML/Graphics.hpp>
+
+namespace cpt
+{
+    
+} // namespace cpt
+
+
+#endif
