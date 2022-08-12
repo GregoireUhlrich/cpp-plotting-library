6bdfe57dc88d869a92c60a44585a3a716826c87d
GregoireUhlrich <greg.um@free.fr>
2022-08-12 17:05:58 +0200

    ticks algorithm 1.0

--- examples/sample_axis.cpp
+++ examples/sample_axis.cpp
@@ -1,29 +1,21 @@
 #include "plotter/axis.hpp"
+#include "plotter/ticks.hpp"
+#include "math_views.hpp"
 
 int main() {
+
+    cpt::print(cpt::calculate_ticks(-3.5f, 0.0f));
+    cpt::print(cpt::calculate_ticks(-1.f, 1.f));
+    cpt::print(cpt::calculate_ticks(0.f, 20.f));
+    cpt::print(cpt::calculate_ticks(1.5f, 5.7f));
+    cpt::print(cpt::calculate_ticks(1e-2f, 3e2f));
+    std::cout.precision(8);
+    cpt::print(cpt::calculate_ticks(1e3f, 1e3f + 1e-2f));
+    return 0;
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!", sf::Style::Close | sf::Style::Resize, settings);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    cpt::Axis right(cpt::Axis::Right, 50.f, {.line_width = 1.f});
-    cpt::Axis left(cpt::Axis::Left,   50.f, {.color = sf::Color::Blue});
-    cpt::Axis up(cpt::Axis::Up,   50.f, {.color = sf::Color::Red});
-    cpt::Axis down(cpt::Axis::Down,   50.f, {.arrow_width = 25.f});
-
-    right.set_position(150.f, 150.f);
-    left.set_position(150.f, 150.f);
-    up.set_position(150.f, 150.f);
-    down.set_position(150.f, 150.f);
-
-    sf::RectangleShape hwitness(sf::Vector2f{100.f, 1.f});
-    hwitness.setPosition(100.f, 157.f);
-    hwitness.setFillColor(sf::Color::Red);
-    sf::RectangleShape vwitness(sf::Vector2f{1.f, 100.f});
-    vwitness.setPosition(157.f, 100.f);
-    vwitness.setFillColor(sf::Color::Red);
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -34,12 +26,6 @@ int main() {
         }
 
         window.clear(sf::Color::White);
-        left.draw(window);
-        right.draw(window);
-        up.draw(window);
-        down.draw(window);
-        window.draw(hwitness);
-        window.draw(vwitness);
         window.display();
     }
 
---
+++ src/plotter/ticks.hpp
@@ -0,0 +1,28 @@
+#ifndef CPT_TICKS_H_INCLUDED
+#define CPT_TICKS_H_INCLUDED
+
+#include "../math_views/array_view.hpp"
+#include "../utils/error.hpp"
+
+namespace cpt
+{
+    class InvalidTicksConfiguration: public cpt::Exception {
+        using cpt::Exception::Exception;
+    };
+
+    struct AutoTicksCongifuration {
+        int min_n_ticks   = 2;
+        int ideal_n_ticks = 6;
+        int max_n_ticks   = 10;
+    };
+
+    cpt::Array<float> calculate_ticks(
+        float xmin, 
+        float xmax,
+        AutoTicksCongifuration const &config = {}
+        );
+        
+} // namespace cpt
+
+
+#endif
